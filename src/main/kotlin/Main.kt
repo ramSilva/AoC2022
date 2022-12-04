71f998ffaadcd91adb79839b149494c7cf5489df
ramsilva <ricardo.silva@sky.uk>
2022-12-04 05:48:05 +0000

    day 4

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,6 +4,9 @@ import puzzles.puzzle2
 import puzzles.puzzle2dot1
 import puzzles.puzzle3
 import puzzles.puzzle3dot1
+import puzzles.puzzle4
+import puzzles.puzzle4alt
+import puzzles.puzzle4dot1
 
 fun main() {
     println("""1: ${puzzle1()}""")
@@ -12,4 +15,7 @@ fun main() {
     println("""2.1: ${puzzle2dot1()}""")
     println("""3: ${puzzle3()}""")
     println("""3.1: ${puzzle3dot1()}""")
+    println("""4: ${puzzle4()}""")
+    println("""4 alt: ${puzzle4alt()}""")
+    println("""4.1: ${puzzle4dot1()}""")
 }
